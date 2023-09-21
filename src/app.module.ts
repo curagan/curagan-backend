e531896316c476cd7bf88238e6ae81f6455415a1
yankyhermawan <yankyhermawan42@gmail.com>
2023-09-21 21:09:02 +0700

    Feat: Set Online Status
    - Create WebSocket gateway for recieve and broadcast list of online doctor
    - Enable CORS method

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DoctorModule } from './doctor/doctor.module';
 import { PatientModule } from './patient/patient.module';
+import { StatusModule } from './status/status.module';
 
 @Module({
-  imports: [DoctorModule, PatientModule],
+  imports: [DoctorModule, PatientModule, StatusModule],
 })
 export class AppModule {}
---
+++ src/status/status.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { StatusService } from './status.service';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class StatusGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+  payloadArray = [];
+  constructor(private readonly statusService: StatusService) {}
+
+  @SubscribeMessage('online')
+  handleConnection(client: Socket, userId: string) {
+    this.sendOnlineData(client, userId);
+  }
+
+  sendOnlineData(client: Socket, userId: string) {
+    if (userId) {
+      this.payloadArray.push(userId);
+      console.log(userId, this.payloadArray);
+      client.emit('online', this.payloadArray);
+      client.broadcast.emit('online', this.payloadArray);
+    }
+  }
+
+  sendOfflineData(
+    client: Socket,
+    payload: { userId: string; isOnline: string },
+  ) {
+    if (payload) {
+      client.broadcast.emit('offline', payload);
+    }
+  }
+}

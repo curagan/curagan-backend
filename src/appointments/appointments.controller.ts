f2c5a42c34794c042f0903f79e9f6936ab2184f4
Nurdin Beta <nurdinbeta@gmail.com>
2023-09-22 22:38:00 +0800

    feat: add swagger
    - add swagger to appointments
    - add swagger to doctor
    - add swagger to patient

--- src/appointments/appointments.controller.ts
+++ src/appointments/appointments.controller.ts
@@ -2,27 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AppointmentsService } from './appointments.service';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
 import { UpdateAppointmentDto } from './dto/update-appointment.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { Appointment } from './entities/appointment.entity';
 
 @Controller('appointments')
+@ApiTags('appointments')
 export class AppointmentsController {
   constructor(private readonly AppointmentsService: AppointmentsService) { }
 
   @Post()
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: Appointment })
   create(@Body() CreateAppointmentDto: CreateAppointmentDto) {
     return this.AppointmentsService.create(CreateAppointmentDto);
   }
 
   @Get()
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: Appointment, isArray: true })
   findAll() {
     return this.AppointmentsService.findAll();
   }
 
   @Get(':id')
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: Appointment })
   findOne(@Param('id') id: string) {
     return this.AppointmentsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: Appointment })
   update(@Param('id') id: string, @Body() updateAppointmentDto: UpdateAppointmentDto) {
     return this.AppointmentsService.update(id, updateAppointmentDto);
   }
